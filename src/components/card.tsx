8fdc5295488f237c228d2f5ef47fdef84a12dee9
madhawa-se <madhawapriyashantha@gmail.com>
2024-05-03 09:50:35 +0530

    add more styling

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,9 +8,11 @@ const Card: React.FC<{ product: Product, type: CardType }> = ({ product: { title
         <article className={`card h-full flex flex-col shadow-md rounded-3xl pt-4 card card--${type}`}>
             <div className='card-content p-4 text-center'>
                 <div className="font-bold text-xl">{title}</div>
-                <figure className="py-4">
-                    <img src={image} className="mx-auto w-full" alt="Example Image" />
-                </figure>
+                <div className="preview-wrap mx-auto">
+                    <figure className="py-4 preview mx-auto">
+                        <img src={image} className="mx-auto w-full h-full preview--img" alt="Example Image" />
+                    </figure>
+                </div>
             </div>
             <div className={`card-footer mt-auto rounded-3xl py-4 px-10 text-center ${type === 'primary' ? 'card--primary bg-primary' : 'card--secondary bg-secondary'}`}>
                 <div className="text-3xl pb-2 text-vividBlue font-bold">{toLKR(price)}</div>
