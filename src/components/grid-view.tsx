f60c31bf22280de7454f770e2725c5f46da0ff86
madhawa-se <madhawapriyashantha@gmail.com>
2024-05-03 16:32:21 +0530

    fix card type

--- src/components/grid-view.tsx
+++ src/components/grid-view.tsx
@@ -1,4 +1,5 @@
 import { useMediaQuery } from "../hooks/media-column-hook";
+import { categoryMap } from "../models/category-map";
 import { Product } from "../models/interfaces/product-interface";
 import { calcWidth } from "../utils/dimention";
 import Card from "./card";
@@ -28,12 +29,15 @@ const GridView: React.FC<GridViewProps> = ({ products, elementsPerRow }) => {
   ]);
 
   const itemWidth = calcWidth(matches.columns);
+  const getCardType = (category: string) => {
+    return (category === categoryMap["womens-clothing"]) ? 'secondary' : 'primary';
+  }
   return (
     <div className="grid-container -mx-4">
       <div className="product-row flex flex-wrap items-stretch">
         {products.map(product => <div key={product.id} className="flex flex-col	items-stretch" style={{ width: itemWidth }}>
           <div className="gap-4 h-full p-4">
-            <Card product={product} type="primary" key={product.id}></Card>
+            <Card product={product} key={product.id} type={getCardType(product.category)} ></Card>
           </div>
         </div>)}
       </div>
